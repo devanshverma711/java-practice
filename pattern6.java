de54dff74f0c1a68cd7d132ed130c2675073796f
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-03-21 10:19:44 +0530

    Add files via upload

---
+++ pattern6.java
@@ -0,0 +1,12 @@
+public class pattern6 {
+    public static void main(String[] args){
+        int u=0;
+        for(int i=1;i<=5;i++){
+            for(int j=0;j<i;j++){
+                u++;
+                System.out.print(u+" ");
+            }
+            System.out.println();
+        }              
+    }
+}
