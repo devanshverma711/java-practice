755b54d6d664f49798050fee4a391f8325266b0f
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-03-31 17:30:08 +0530

    Preety Writing

---
+++ preetywriting.java
@@ -0,0 +1,14 @@
+public class preetywriting{
+    public static void main(String[] args){
+        float a = 453.1274f;
+        System.out.printf("Formatted number is %.3f",a);
+
+        System.out.println("");
+
+        System.out.printf("Pie: %.3f",Math.PI);
+
+        System.out.println("");
+
+        System.out.printf("Hello my name is %s and I am %s", "Devansh","Cool");
+    }
+}
