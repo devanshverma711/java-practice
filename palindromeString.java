1ff0b3c292f505b1326ca36937a4963f7bb81d61
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-03-31 18:07:30 +0530

    Check String is Palindrome

---
+++ palindromeString.java
@@ -0,0 +1,20 @@
+public class palindromeString {
+    public static void main(String[] args){
+        String str = "a";
+        System.out.println(ispalindrome(str));
+    }
+    static boolean ispalindrome(String str){
+        if(str.length()==0){
+            return true;
+        }
+        for(int i=0;i<str.length()/2;i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+
+            if(start!=end){
+                return false;
+            }
+        }
+        return true;
+    }
+}
