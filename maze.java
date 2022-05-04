57d72e9abea3365b855849fa9d226c39c2f0c9e3
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-05-04 22:52:27 +0530

    maze path count including diagonals

--- maze.java
+++ maze.java
@@ -2,7 +2,8 @@
 
 public class maze {
     public static void main(String[] args){
-        System.out.println(maze(3,3));
+       // System.out.println(maze(3,3));
+        path("",3,3);
     }
     static int maze(int r,int c){
         if(r==1 || c==1){
@@ -12,5 +13,23 @@ public class maze {
         int left = maze(r-1,c);
         int right = maze (r,c-1);
         return left +right;    
+    }
+
+    static void path(String p, int r, int c){
+        if(r==1 && c==1){
+            System.out.print(p +" ");
+            return;
+        }
+        if(r>1 && c>1){
+            path(p+'D', r-1, c-1);
+        }
+        
+        if(r>1){
+            path(p+'V', r-1, c);
+        }
+        if(c>1){
+            path(p+'H',r,c-1);
+        }
+
     }
 }
