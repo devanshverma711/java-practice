d52403768cc7482fa55abbe47ad264a670723d32
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-05-04 19:37:36 +0530

    counting path of maze

---
+++ maze.java
@@ -0,0 +1,16 @@
+// maze cout of paths
+
+public class maze {
+    public static void main(String[] args){
+        System.out.println(maze(3,3));
+    }
+    static int maze(int r,int c){
+        if(r==1 || c==1){
+            return 1;
+        }
+
+        int left = maze(r-1,c);
+        int right = maze (r,c-1);
+        return left +right;    
+    }
+}
