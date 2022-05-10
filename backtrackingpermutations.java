f0c5cc44669ecf18f92ea529bf2e58dba1875f3f
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-05-10 06:05:14 +0530

    PERMUTATION

---
+++ backtrackingpermutations.java
@@ -0,0 +1,19 @@
+public class backtrackingpermutations {
+    public static void main(String[] args) {
+        printpermutation("ABC", "", 0);
+    }
+
+    static void printpermutation(String str,String perm,int idx){
+        if(str.length()==0){
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i=0;i<str.length();i++){
+            char curr = str.charAt(i);
+            String newstr = str.substring(0,i)+str.substring(i+1);
+            printpermutation(newstr, perm+curr, idx+1);
+        }
+
+    }
+}
