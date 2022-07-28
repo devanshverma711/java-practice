fbca6a9f74c2e347b6465a67fd458d05d62277b3
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-07-28 21:07:08 +0530

    Add files via upload
    read until end of file;

---
+++ EOF.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class datatypess {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+        while(sc.hasNext()){
+            String str = sc.nextLine();
+            System.out.println(i+" " +str);
+            i++;
+        }
+    }
+}
+
