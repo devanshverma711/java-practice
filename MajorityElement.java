321bb1f477f947293597cbd085d5ddbfd7592015
DEVANSH VERMA <68294758+devanshverma711@users.noreply.github.com>
2022-08-03 12:05:51 +0530

    Majority Element
    Given an array nums of size n, return the majority element.
    
    The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.

---
+++ MajorityElement.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of Array");
+        int n = sc.nextInt();
+        int[] digits = new int[n];
+        System.out.println("Enter digits of Array");
+        for(int i=0;i<n;i++){
+            digits[i]=sc.nextInt();
+        }
+        System.out.println("Array is -- ");
+        for(int i=0;i<n;i++){
+            System.out.print(digits[i] +" ");
+        }
+        System.out.println();
+        count(digits);
+    }
+    public static void count(int[] digits){
+        boolean visited[] = new boolean[digits.length];
+
+        for (int i = 0; i < digits.length; i++) {
+
+            // Skip this element if already processed
+            if (visited[i] == true)
+                continue;
+
+            // Count frequency
+            int count = 1;
+            for (int j = i + 1; j < digits.length; j++) {
+                if (digits[i] == digits[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+            if(count>(digits.length/2))
+            System.out.println(digits[i] );
+        }
+    }
+}
